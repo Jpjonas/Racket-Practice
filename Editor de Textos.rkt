14697f03803005c6e07171afea7ee933c039c14f
Jonas Pollnow <jpjonas35@gmail.com>
2017-05-25 15:54:43 -0300

    Update Editor de textos
    Yo incluí la funcio string-remove-last, vos tenés que revisar la
    etiqueta.
    Y también cambié el nombre de la función agregarCaracter, para
    keyboardHandler, pues es una función mas genérica que sólo agregar
    caracteres.

--- Editor de Textos.rkt
+++ Editor de Textos.rkt
@@ -18,9 +18,24 @@
 ;Definición del estado inicial del sistema
 (define INICIAL (make-texto "" "indigo" 20))
 
-;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) mediante la estructura "texto" y un color con un string.
+;Borra la ultima letra de la cadena
+;string-remove-last: String -> String
+;Recibe una String, devuelve la misma String sin la ultima letra
+;(check-expect (string-remove-last "hola") "hol")
+;(check-expect (string-remove-last "hoy") "ho")
+;(check-expect (string-remove-last "mundo") "mund")
+(define (string-remove-last s)
+  (if (not(string=? "" s))
+      (substring s 0 (- (string-length s) 1))
+      s))
+
+;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color)
+;mediante la estructura "texto" y un color con un string.
 ;cambiaColor: texto String -> texto
 ;Cambia el elemento color de una estructura "texto" f1, f2, f3, f4, f5 y f6.
+;(check-expect (cambiaColor (make-texto "hola" "red" 20) "f3") (make-texto "hola" "green" 20))
+;(check-expect (cambiaColor (make-texto "" "black" 20) "f1") (make-texto "" "black" 20))
+;(check-expect (cambiaColor (make-texto "-mundo" "indigo" 20) "f5") (make-texto "-mundo" "indigo" 20))
 (define (cambiaColor n k)
   (cond [(key=? k "f1") (make-texto (texto-cadena n) "black" (texto-tam n))]
         [(key=? k "f2") (make-texto (texto-cadena n) "red" (texto-tam n))]
@@ -30,50 +45,54 @@
         [(key=? k "f6") (make-texto (texto-cadena n) "grey" (texto-tam n))]
         [else n]))
 
-(check-expect (cambiaColor (make-texto "hola" "red" 20) "f3") (make-texto "hola" "green" 20))
-(check-expect (cambiaColor (make-texto "" "black" 20) "f1") (make-texto "" "black" 20))
-(check-expect (cambiaColor (make-texto "-mundo" "indigo" 20) "5") (make-texto "-mundo" "indigo" 20))
-
-;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) con la estructura "texto" y la tecla precionada con un string.
+;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) con la estructura
+;"texto" y la tecla precionada con un string.
 ;cambiaTamañoFuente: texto String -> texto
 ;Cambia el tamaño de la fuente con las teclas "up" y "down"
+;(check-expect (cambiaTamañoFuente (make-texto "test" "black" 20) "up") (make-texto "test" "black" 21))
+;(check-expect (cambiaTamañoFuente (make-texto "tset" "black" 12) "down") (make-texto "tset" "black" 11))
+;(check-expect (cambiaTamañoFuente (make-texto "42" "black" 1) "down") (make-texto "42" "black" 1))
 (define (cambiaTamañoFuente n k)
   (cond[(key=? k "up") (make-texto (texto-cadena n) (texto-color n) (+ (texto-tam n) 1))]
-       [(key=? k "down") (make-texto (texto-cadena n) (texto-color n) (if (< 1 (- (texto-tam n) 1)) (- (texto-tam n) 1) (texto-tam n)))]))
+       [(key=? k "down") (make-texto (texto-cadena n)
+                                     (texto-color n)
+                                     (if (< 1 (- (texto-tam n) 1))
+                                         (- (texto-tam n) 1)
+                                         (texto-tam n)))]))
 
-(check-expect (cambiaTamañoFuente (make-texto "test" "black" 20) "up") (make-texto "test" "black" 21))
-(check-expect (cambiaTamañoFuente (make-texto "tset" "black" 12) "down") (make-texto "tset" "black" 11))
-(check-expect (cambiaTamañoFuente (make-texto "42" "black" 1) "down") (make-texto "42" "black" 1))
-
-;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) con la estructura "texto" y la pantalla del editor de texto mediante una imagen.
+;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) con la estructura
+;"texto" y la pantalla del editor de texto mediante una imagen.
 ;pantalla : texto -> Image
 ;Imprime el string de la variable "texto" en una imagen.
 (define (pantalla n)
   (place-image/align (text (texto-cadena n) (texto-tam n) (texto-color n))
                      0 0 "left" "top" FONDO))
 
-;Representa las características del texto (contenido, tamaño de la fuente y color) con la estructura "texto" y la tecla precionada con un string.
-;agregarCaracter : texto string -> texto
-;Agrega los caracteres digitados a la cadena, borra caracteres con la tecla backspace (en el caso que la cadena no sea vacía), determina el tamanho de fuente con "up" y "down" y cambia el color de la cadena.
-(define (agregarCaracter n k)
+;Representa las características del texto (contenido, tamaño de la fuente y color) con la
+;estructura "texto" y la tecla precionada con un string.
+;keyboardHandler : texto string -> texto
+;Agrega los caracteres digitados a la cadena, borra caracteres con la tecla backspace
+;(en el caso que la cadena no sea vacía), determina el tamanho de fuente con "up" y "down"
+;y cambia el color de la cadena.
+;(check-expect (keyboardHandler (make-texto "" "indigo" 20) "a") (make-texto "a" "indigo" 20))
+;(check-expect (keyboardHandler (make-texto "Buenas" "indigo" 20) " ") (make-texto "Buenas " "indigo" 20))
+;(check-expect (keyboardHandler (make-texto "Winter is Coming" "indigo" 20) "f1") (make-texto "Winter is Coming" "black" 20))
+;(check-expect (keyboardHandler (make-texto "qsy" "indigo" 20) "\b") (make-texto "qs" "indigo" 20))
+;(check-expect (keyboardHandler (make-texto "" "indigo" 20) "\b") (make-texto "" "indigo" 20))
+(define (keyboardHandler n k)
   (cond
     ;Determina el tamaño de la fuente
-    [(or (key=? k "up") (key=? k "down")) (cambiaTamañoFuente n k)]
+    [(or (key=? k "up") (key=? k "down"))
+     (cambiaTamañoFuente n k)]
     ;Determina el color de la fuente
-    [(or (key=? k "f1") (key=? k "f2") (key=? k "f3") (key=? k "f4") (key=? k "f5") (key=? k "f6")) (cambiaColor n k)]
+    [(or (key=? k "f1") (key=? k "f2") (key=? k "f3") (key=? k "f4") (key=? k "f5") (key=? k "f6"))
+     (cambiaColor n k)]
     ;Borra las letras con la tecla "backspace"
-    [(and (key=? k "\b") (> (string-length (texto-cadena n)) 0))
-     (make-texto (substring (texto-cadena n) 0 (- (string-length (texto-cadena n)) 1)) (texto-color n) (texto-tam n))]
+    [(key=? k "\b")
+     (make-texto (string-remove-last (texto-cadena n)) (texto-color n) (texto-tam n))]
     ;Adiciona las letras precionadas a la cadena del estado
-    [(not (key=? k "\b")) (make-texto (string-append (texto-cadena n) k) (texto-color n) (texto-tam n))]
-    [else n]))
-
-(check-expect (agregarCaracter (make-texto "" "indigo" 20) "a") (make-texto "a" "indigo" 20))
-(check-expect (agregarCaracter (make-texto "Buenas" "indigo" 20) " ") (make-texto "Buenas " "indigo" 20))
-(check-expect (agregarCaracter (make-texto "Winter is Coming" "indigo" 20) "f1") (make-texto "Winter is Coming" "black" 20))
-(check-expect (agregarCaracter (make-texto "qsy" "indigo" 20) "\b") (make-texto "qs" "indigo" 20))
-(check-expect (agregarCaracter (make-texto "" "indigo" 20) "\b") (make-texto "" "indigo" 20))
+    [else (make-texto (string-append (texto-cadena n) k) (texto-color n) (texto-tam n))]))
 
 (big-bang INICIAL 
           [to-draw pantalla]          ;Diseño de la pantalla
-          [on-key agregarCaracter])   ;Agregar caracter a la cadena
+          [on-key keyboardHandler])   ;Agregar caracter a la cadena
