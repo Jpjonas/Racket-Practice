d7258e902d0f31dca33fb100b1ae3d7abc31ce3e
originalnicodr <niconavall@gmail.com>
2017-05-25 16:49:08 -0300

    Update Editor de Textos.rkt
    Un par de cositas en los comentarios, nada especial.

--- Editor de Textos.rkt
+++ Editor de Textos.rkt
@@ -2,6 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname |Editor de Textos|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "docs.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "docs.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 ;EDITOR DE TEXTOS
+;Alumnos: Navall, Nicolás Uriel - Pollnow, Jonas
 
 ;Un texto es: (make-texto String Color Number)
 ;El primer elemento es la cadena a mostrarse
@@ -18,7 +19,7 @@
 ;Definición del estado inicial del sistema
 (define INICIAL (make-texto "" "indigo" 20))
 
-;Borra la ultima letra de la cadena
+;Representa un texto mediante un string
 ;string-remove-last: String -> String
 ;Recibe una String, devuelve la misma String sin la ultima letra
 ;(check-expect (string-remove-last "hola") "hol")
@@ -30,9 +31,9 @@
       s))
 
 ;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color)
-;mediante la estructura "texto" y un color con un string.
+;mediante la estructura "texto" y un color con un string
 ;cambiaColor: texto String -> texto
-;Cambia el elemento color de una estructura "texto" f1, f2, f3, f4, f5 y f6.
+;Cambia el elemento color de una estructura "texto" f1, f2, f3, f4, f5 y f6
 ;(check-expect (cambiaColor (make-texto "hola" "red" 20) "f3") (make-texto "hola" "green" 20))
 ;(check-expect (cambiaColor (make-texto "" "black" 20) "f1") (make-texto "" "black" 20))
 ;(check-expect (cambiaColor (make-texto "-mundo" "indigo" 20) "f5") (make-texto "-mundo" "indigo" 20))
@@ -46,7 +47,7 @@
         [else n]))
 
 ;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) con la estructura
-;"texto" y la tecla precionada con un string.
+;"texto" y la tecla precionada con un string
 ;cambiaTamañoFuente: texto String -> texto
 ;Cambia el tamaño de la fuente con las teclas "up" y "down"
 ;(check-expect (cambiaTamañoFuente (make-texto "test" "black" 20) "up") (make-texto "test" "black" 21))
@@ -61,15 +62,15 @@
                                          (texto-tam n)))]))
 
 ;Representa las caracteristicas del texto (contenido, tamaño de la fuente y color) con la estructura
-;"texto" y la pantalla del editor de texto mediante una imagen.
+;"texto" y la pantalla del editor de texto mediante una imagen
 ;pantalla : texto -> Image
-;Imprime el string de la variable "texto" en una imagen.
+;Imprime el string de la variable "texto" en una imagen
 (define (pantalla n)
   (place-image/align (text (texto-cadena n) (texto-tam n) (texto-color n))
                      0 0 "left" "top" FONDO))
 
 ;Representa las características del texto (contenido, tamaño de la fuente y color) con la
-;estructura "texto" y la tecla precionada con un string.
+;estructura "texto" y la tecla precionada con un string
 ;keyboardHandler : texto string -> texto
 ;Agrega los caracteres digitados a la cadena, borra caracteres con la tecla backspace
 ;(en el caso que la cadena no sea vacía), determina el tamanho de fuente con "up" y "down"
